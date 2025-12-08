920a02707a8841ce625acaf5f6781bc5aa796540
Michail Kostochka <kma03.12.2002@gmail.com>
2025-12-08 23:37:38 +0300

    Return wheels

--- build_package.sh
+++ build_package.sh
@@ -1,8 +1,5 @@
 mkdir -p RPM/{RPMS,SPECS,SOURCES}
 
-mkdir backend/wheels
-pip3 wheel --wheel-dir=./backend/wheels -r ./backend/requirements.txt
-
 tar --transform 's,^backend,ankup-1.0.0,' --exclude='Dockerfile' --exclude='requirements.txt' -czf ankup-1.0.0.tar.gz backend/
 
 cp specs/*.spec RPM/SPECS/
