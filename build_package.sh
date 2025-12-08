9970e13132a255f04491e98d0ce33839177eff81
Michail Kostochka <kma03.12.2002@gmail.com>
2025-12-08 23:43:40 +0300

    fix build script

--- build_package.sh
+++ build_package.sh
@@ -1,11 +1,11 @@
 set -x
 
-mkdir -p RPM/{RPMS,SPECS,SOURCES}
+mkdir -p ~/RPM/{RPMS,SPECS,SOURCES}
 
 tar --transform 's,^backend,ankup-1.0.0,' --exclude='Dockerfile' --exclude='requirements.txt' -czf ankup-1.0.0.tar.gz backend/
 
-cp specs/*.spec RPM/SPECS/
-cp services/*.service RPM/SOURCES/
-cp ankup-1.0.0.tar.gz RPM/SOURCES/
+cp specs/*.spec ~/RPM/SPECS/
+cp services/*.service ~/RPM/SOURCES/
+cp ankup-1.0.0.tar.gz ~/RPM/SOURCES/
 
-rpmbuild -bb RPM/SPECS/ankup.spec
+rpmbuild -bb ~/RPM/SPECS/ankup.spec
